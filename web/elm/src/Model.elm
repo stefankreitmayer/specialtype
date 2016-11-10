a156baebd5b14979b9406accc1080a3c75c475d8
Stefan Kreitmayer <stefankreitmayer@gmail.com>
2016-11-11 00:50:32 +0100

    add profile model and view

--- web/elm/src/Model.elm
+++ web/elm/src/Model.elm
@@ -1,13 +1,31 @@
 module Model exposing (..)
 
-import Date exposing (Date)
+import Dict exposing (Dict)
+import String
+import List
 
 import Model.Page exposing (Page(..))
 
 type alias Model =
-  { currentPage : Page }
+  { currentPage : Page
+  , profile : Profile }
+
+type alias Profile =
+  Dict Char Score
+
+type alias Score =
+  { hits : Int
+  , misses : Int }
 
 
 initialModel : Model
 initialModel =
-  { currentPage = Home }
+  { currentPage = Home
+  , profile = emptyProfile }
+
+
+emptyProfile : Profile
+emptyProfile =
+  "!@£$%^&*()-_=+[]{};:'\"\\`~,./<>?"
+  |> String.toList
+  |> List.foldr (\char dict -> Dict.insert char (Score 0 0) dict) Dict.empty
--- web/elm/src/View/Pages/Home.elm
+++ web/elm/src/View/Pages/Home.elm
@@ -1,8 +1,11 @@
 module View.Pages.Home exposing (view)
 
-import Html exposing (Html,div,h1,button)
+import Html exposing (Html,div,h1,button,table,tr,td)
 import Html.Attributes exposing (id,class)
 import Html.Events exposing (onClick)
+
+import Dict
+import String
 
 import Model exposing (..)
 import Model.Page exposing (Page(..))
@@ -12,8 +15,37 @@ import Msg exposing (..)
 
 view : Model -> Html Msg
 view model =
-  div
-    []
-    [ h1 [] [ Html.text "Home" ]
-    , button [ onClick (Navigate Play) ]
-      [ Html.text "Play" ] ]
+  div []
+    [ Html.main' [] (viewMainContent model)
+    , Html.footer [] [ viewProfile model ]
+    ]
+
+
+viewMainContent : Model -> List (Html Msg)
+viewMainContent model =
+  [ h1 [] [ Html.text "Special Type" ]
+  , button [ onClick (Navigate Play) ]
+    [ Html.text "Play" ]
+  ]
+
+
+viewProfile : Model -> Html Msg
+viewProfile {profile} =
+  let
+      chars = Dict.keys profile |> List.map (\char -> cell "Char" (String.cons char ""))
+      hitcounts = Dict.values profile |> List.map (\{hits} -> hits |> toString |> cell "Hits")
+      misscounts = Dict.values profile |> List.map (\{misses} -> misses |> toString |> cell "Misses")
+  in
+      table
+        [ class "Profile__Board" ]
+        [ (tr [] chars)
+        , (tr [] hitcounts)
+        , (tr [] misscounts)
+        ]
+
+
+cell : String -> String -> Html Msg
+cell modifier str =
+  td
+    [ class ("Profile__Cell Profile__Cell--" ++ modifier) ]
+    [ Html.text str ]
